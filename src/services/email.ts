87c901e525df0fba75c79c1cfa40e4910f758b1f
ygowda22 <84380388+ygowda22@users.noreply.github.com>
2026-04-29 18:52:32 -0500

    Change email transport settings to use port 587
    email port changes

--- src/services/email.ts
+++ src/services/email.ts
@@ -1,9 +1,9 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host:   process.env.EMAIL_HOST   || "smtp.gmail.com",
-  port:   Number(process.env.EMAIL_PORT) || 465,
-  secure: true,// REQUIRED for port 465
+ host:   process.env.EMAIL_HOST   || "smtp.gmail.com",
+  port:   Number(process.env.EMAIL_PORT) || 587,
+  secure: false,// REQUIRED for port 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
